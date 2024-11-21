c06457ff8838420bef5dc31de1f4771c29c01a8f
santiago emanuel mustafa font <santiagomustafa3@gmail.com>
2024-11-20 22:50:41 -0300

    site Vercel Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,15 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), react()],
+  adapter: vercel(
+     webAnalytics: {
+      enabled: true,
+    },
+  ),
 });
