6fd166bb49fbe4b6963cf98babc93cbcd7d158e2
santiago emanuel mustafa font <santiagomustafa3@gmail.com>
2024-11-27 18:44:39 -0300

    set default export

--- src/lib/mailjet.js
+++ src/lib/mailjet.js
@@ -1,6 +1,8 @@
 import Mailjet from "node-mailjet";
 
-export const mail = Mailjet.apiConnect(
+const mail = Mailjet.apiConnect(
   import.meta.env.PUBLIC_KEY,
   import.meta.env.PUBLIC_SECRET
 );
+
+export default mail;
